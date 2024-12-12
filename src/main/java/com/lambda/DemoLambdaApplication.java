6ec201d75f3e8e487ac8c2e173003800b70bd540
Tam Nguyen <tamnct1999@gmail.com>
2024-12-13 00:38:04 +0700

    first commit

---
+++ src/main/java/com/lambda/DemoLambdaApplication.java
@@ -0,0 +1,13 @@
+package com.lambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLambdaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/lambda/handler/CreateEmployeeHandler.java
@@ -0,0 +1,21 @@
+package com.lambda.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.lambda.entity.EmployeeEntity;
+
+public class CreateEmployeeHandler implements RequestHandler<EmployeeEntity, String>{
+		
+	private static final Logger log = LoggerFactory.getLogger(CreateEmployeeHandler.class);
+
+	@Override
+	public String handleRequest(EmployeeEntity event, Context context) {
+		log.info("Received a request in your lambda  function {} with details {}", context.getFunctionName(), event);
+		
+		return "Employee is created successfully !";
+	}
+ 
+}
